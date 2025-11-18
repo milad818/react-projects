06e57abc26f68a798e965cb05b9016d29daafdb6
Milad Zakhireh <eng.zakhireh@gmail.com>
2025-11-18 10:10:07 +0100

    Test clicking 'Add to Cart' button (display, post, reload)

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,8 +1,14 @@
 
 
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';  // lets us simulate events e.g., a click
 import { expect, it, describe, vi } from 'vitest';  // renders a component in a fake web page
 import { Product } from './Product';
+import axios from 'axios';
+
+
+// Mock entire axios package
+vi.mock('axios');
 
 
 // Component testing requires further libraries to be installed
@@ -52,6 +58,44 @@ describe('Product Component', () => {
     // Test rating count
     expect(
       screen.getByText('127')
-    ).toBeInTheDocument();
+    ).toBeInTheDocument();    
+  });
+
+
+  // Test 'Add to Cart' button
+  it('adds product to the cart as expected', async () => {
+    const product = {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: ["sports", "basketballs"]
+  }
+
+    // Mock for loadCart (creates a fake function)
+    const loadCart = vi.fn();
+
+    render(<Product product={product} loadCart={loadCart} />)
+
+    const user = userEvent.setup();
+    const addToCartButton = screen.getByTestId('add-to-cart-button')
+    await user.click(addToCartButton);   // simulates the click event
+
+    // Check if axios.post is called and values are passed correctly
+    expect(axios.post).toHaveBeenCalledWith(
+      '/api/cart-items',
+      {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+      }
+    );
+
+    // Check if our code ran loadCart
+    // Remember that in the corresponding async func, firt posted and then cart re-loaded
+    expect(loadCart).toHaveBeenCalled();
   });
 });
